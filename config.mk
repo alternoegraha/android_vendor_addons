eb368734b383ba3da8a0c777b97567c4c592c1a0
alternoegraha <noegrahachan@gmail.com>
2025-08-23 09:08:47 +0000

    addons: drop SF Display font
    - Will be replaced by Inter 4.1
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- config.mk
+++ config.mk
@@ -63,7 +63,6 @@ PRODUCT_PACKAGES += \
     FontReemKufiOverlay \
     FontRobotoCondensedOverlay \
     FontRubikOverlay \
-    FontSanFranciscoDisplayProSourceOverlay \
     FontSamsungOneOverlay \
     FontSonySketchOverlay \
     FontStoropiaOverlay \
